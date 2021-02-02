ebab5b1705ec742ba7f99ab6e57a5898407d0521
Kevin <millhousekevin@gmail.com>
2021-02-01 22:35:51 -0500

    fixed hours for styling logic

--- script.js
+++ script.js
@@ -1,66 +1,65 @@
 var container = document.querySelector(".container");
 var date = document.getElementById("currentDay");
-var currentHour = (moment().startOf("hour").format("ha"));
-var time = [];
+// var currentHour = (moment().startOf("hour").format("ha"));
 var saveBtn = document.querySelector(".saveBtn");
 date.textContent = moment().format("dddd, MMMM Do, YYYY");
-console.log(currentHour)
+var current = (moment().hour())
+
 timeOneDay();
+// idfortime();
 
 function timeOneDay() { //generates strings of the indivdual hours based off of the start of the day and pushes them into an array
-    for (i = 0; i < 25; i++) {
-        var formattedTime = moment().startOf("day").add(i, "hours").format("ha");
-        time.push(formattedTime);
-    } makeBlx();
-}
-
-function makeBlx() {
-
-    for (var i = 0; i < time.length - 1; i++) { //Loops through length of time array 
-        
-        var hrColTxt = time[i];// sets the value of hrColTxt to the string corresponding to the index value in the array being looped through
+    for (i = 0; i < 24; i++) {
+        var hours = moment().startOf("day").add(i, "hours").format("H")
+        // console.log(hours)
+        var formattedTime = moment().startOf("day").add(i, "hours").format("ha").toString();
+        // console.log(formattedTime)
+        // time.push(formattedTime);
+        var hrColTxt = formattedTime;// sets the value of hrColTxt to the string corresponding to the index value in the array being looped through
+    
         var rowCon = document.createElement("div");
-        var hrCols = document.createElement("div");//creates column that holds hours
-        var textarea = document.createElement("textarea");//creates textarea element
-        var saveBtn = document.createElement("button");//creates save button
-        var clrBtn = document.createElement("button");
-        hrCols.textContent = hrColTxt;//sets the textcontent of hrCols div to index string corresponding to hrColTxt
-        saveBtn.textContent = "Save";//sets starting text of button
-        clrBtn.textContent = "Clear";
-        rowCon.setAttribute("class", "row time-block mb-5")
+        rowCon.setAttribute("class", "row time-block")
+        rowCon.setAttribute("id", hours)
+       
+        var hrCols = document.createElement("div");//creates column for hour text
         hrCols.setAttribute("class", "col hour");
-        textarea.setAttribute("class", "col-9  future description");
+
+        var textarea = document.createElement("textarea");//creates textarea element
+        textarea.setAttribute("class", "col-9  present description");
+        
+        var saveBtn = document.createElement("button");//creates save button
+        saveBtn.textContent = "Save";
         saveBtn.setAttribute("class", "col  saveBtn ");
+
+        var clrBtn = document.createElement("button");
+        clrBtn.textContent = "Clear";
         clrBtn.setAttribute("class", "col  clrBtn" )
 
         container.appendChild(rowCon);
+        rowCon.appendChild(clrBtn);
         rowCon.appendChild(hrCols);
         rowCon.appendChild(textarea);
-        rowCon.appendChild(clrBtn);
         rowCon.appendChild(saveBtn);
 
-        
-        if(!localStorage.getItem(hrCols.textContent)){
-            localStorage.setItem(hrCols.textContent, "")
-        }
-        else{
-            var todo = localStorage.getItem(hrCols.textContent);
-            textarea.value = todo; 
-        };
+        hrCols.textContent = hrColTxt;
+        console.log(hrColTxt);
+        console.log(rowCon.id);        
 
-        if (hrColTxt === currentHour ) {
-            textarea.setAttribute("class", "col-9  present description")
-        }
-        if ((hrColTxt) < currentHour) {
+        if (parseInt(rowCon.id) < current) {
             textarea.setAttribute("class", "col-9  past description")
+        }
+
+        else if (parseInt(rowCon.id) > current) {
+            textarea.setAttribute("class", "col-9  future description")
            
         }
-        
+
         saveBtn.addEventListener("click", function (event) {
             event.preventDefault();
-        
-         var hour = event.target.parentNode.textContent;
-         var textarea = event.target.previousSibling;
+            var textarea = event.target.previousSibling;
+            var hour = textarea.previousSibling.textContent;
+            var row = event.target.parentNode;
+            // console.log(row.id)
          var text = textarea.value;
          if (text == "") {
             alert("must add text to save")
@@ -68,5 +67,9 @@ function makeBlx() {
          localStorage.setItem(hour, text);
 
          });
-    };
+        
+    } 
+  
 }
+
+
